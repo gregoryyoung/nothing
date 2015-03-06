297f48afdc3535be5a7a8ad439f150dd6016cf21
Greg Young <gregoryyoung1@gmail.com>
2015-03-06 12:47:26 -0800

    will never touch this again

---
+++ Unit/Nothing.cs
@@ -0,0 +1,17 @@
+﻿namespace Unit
+{
+    /// <summary>
+    /// This class should be used instead of void for return types.
+    /// </summary>
+    public class Nothing
+    {
+        public static Nothing Instance = new Nothing();
+
+        private Nothing() { }
+
+        public override string ToString()
+        {
+            return "Nothing";
+        }
+    }
+}
