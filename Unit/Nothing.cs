745c452aeabb5ffa54c6f65e384e535283e5c34e
Don Syme <dsyme@users.noreply.github.com>
2021-07-28 12:52:26 +0100

    Spiking a new approach

--- Unit/Nothing.cs
+++ Unit/Nothing.cs
@@ -1,17 +1,1 @@
-﻿namespace Unit
-{
-    /// <summary>
-    /// This class should be used instead of void for return types.
-    /// </summary>
-    public class Nothing
-    {
-        public static readonly Nothing Instance = new Nothing();
 
-        private Nothing() { }
-
-        public override string ToString()
-        {
-            return "Nothing";
-        }
-    }
-}
